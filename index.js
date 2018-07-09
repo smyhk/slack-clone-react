262bc1270fad7c0720d5d86217827c970c5bc591
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-07-08 20:21:42 -0700

    Setup graphql with Apollo server

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
+
+import typeDefs from './schema';
+import resolvers from './resolvers';
+
+export const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+//const myGraphQLSchema = // ... define or import your schema here!
+const PORT = 8080;
+
+const app = express();
+
+// bodyParser is needed just for POST.
+app.use(
+  '/graphql',
+  bodyParser.json(),
+  graphqlExpress({ schema: schema })
+);
+
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+app.listen(PORT, () => {
+  console.info(`Server listening on port ${PORT}`);
+});
