caaab85c58bb432c844a3fd37385a6b6759d007e
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-07-13 14:26:30 -0700

    Add members to a team

--- src/resolvers/team.js
+++ src/resolvers/team.js
@@ -9,6 +9,58 @@ export default {
     )
   },
   Mutation: {
+    addTeamMember: requiresAuth.createResolver(
+      async (parent, { email, teamId }, { models, user }) => {
+        try {
+          const teamPromise = models.findOne(
+            { where: { id: teamId } },
+            { raw: true }
+          );
+          const userToAddPromise = models.findOne(
+            { where: { email } },
+            { raw: true }
+          );
+          const [team, userToAdd] = await Promise.all([
+            teamPromise,
+            userToAddPromise
+          ]);
+
+          if (team.owner !== user.id) {
+            return {
+              ok: false,
+              errors: [
+                {
+                  path: 'email',
+                  message: 'Only the team owner may add members'
+                }
+              ]
+            };
+          }
+
+          if (!userToAdd) {
+            return {
+              ok: false,
+              errors: [
+                {
+                  path: 'email',
+                  message: 'Coudld not find user with this email'
+                }
+              ]
+            };
+          }
+          await models.Member.create({ userId: userToAdd.id, teamId });
+          return {
+            ok: true
+          };
+        } catch (err) {
+          console.info(err);
+          return {
+            ok: false,
+            errors: formatErrors(err)
+          };
+        }
+      }
+    ),
     createTeam: requiresAuth.createResolver(
       async (parent, args, { models, user }) => {
         try {
