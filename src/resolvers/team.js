abfeb2513ea0a53f8b07dde904faf0904038521c
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-07-16 10:33:37 -0700

    Optimize inviteTeams query

--- src/resolvers/team.js
+++ src/resolvers/team.js
@@ -9,16 +9,12 @@ export default {
     ),
     inviteTeams: requiresAuth.createResolver(
       async (parent, args, { models, user }) =>
-        models.Team.findAll(
+        models.sequelize.query(
+          'select * from teams join members on id = team_id where user_id = ?',
           {
-            include: [
-              {
-                model: models.User,
-                where: { id: user.id }
-              }
-            ]
-          },
-          { raw: true }
+            replacements: [user.id],
+            model: models.Team
+          }
         )
     )
   },
